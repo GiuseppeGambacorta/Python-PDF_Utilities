0fb79f662d911145d4f95acc2eb34b64ca5ee2e5
giuseppe gambacorta <gambacortagiuseppe@oulook.it>
2024-10-30 21:47:05 +0100

    Removes update

--- RemovePages.py
+++ RemovePages.py
@@ -2,43 +2,39 @@ import PyPDF2
 import sys
 
 
-
-
 length_of_input_args = len(sys.argv)
 
-if length_of_input_args <=1:
-    print('Also give a PDF filepath on which script will work\n')
+if length_of_input_args == 1:
+    print('Also give the original pdf file name and the ouput file name\n')
     exit(1)
-else:
-    path_of_input_file= sys.argv[1]
-    if length_of_input_args==3: # To give output file path
-	    out_fpath = sys.argv[2]
-    elif length_of_input_args==2:   # Replace the same file
-	    out_fpath = path_of_input_file
+elif length_of_input_args == 2:
+    print('Also give  the output file name\n')
+    exit(1)
 
 
-in_file = PyPDF2.PdfReader(path_of_input_file, 'rb') 
+input_file_name = sys.argv[1]
+output_file_name = sys.argv[2]
+
+
+in_file = PyPDF2.PdfReader(input_file_name, 'rb')
 out_file = PyPDF2.PdfWriter()
 
 
-pages_to_delete = [1,2,3,4]
-
+pages_to_delete = [1, 2, 3, 4]
 
 if len(pages_to_delete) > len(in_file.pages):
     print('Pages to delete are more than pages in PDF')
     exit(1)
 
 index_to_delete = [i-1 for i in pages_to_delete]
-keep_pages = [page for i, page in enumerate(in_file.pages) if i not in index_to_delete]
+pages_to_keep = [page for i, page in enumerate(in_file.pages) if i not in index_to_delete]
 
-for page in keep_pages:
-     out_file.add_page(page)  
-
-
-print(len(in_file.pages) - len(out_file.pages), 'pages to be deleted')
+for page in pages_to_keep:
+    out_file.add_page(page)
 
 
+print(len(in_file.pages) - len(out_file.pages), 'pages deleted')
 
 
-with open(out_fpath, 'wb') as f:
+with open(output_file_name, 'wb') as f:
     out_file.write(f)
